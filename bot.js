da9f0328b9281c6e93d19974470a4de17d803ab1
DRpiow <49477059+DRpiow@users.noreply.github.com>
2019-04-10 19:44:39 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'HI') {
-        message.reply('HI I Dark-Hexagon');
-      }
+// Set the prefix
+const prefix = "d!";
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (d!message.content.startsWith(prefix)) return;
+ 
+  if (message.content.startsWith(prefix + "HI")) {
+    message.channel.send("HI I DARK HEXAGON!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
