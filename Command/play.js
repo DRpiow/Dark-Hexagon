106b45b02fef870ecb3728da2d095949ba9d7b4d
DRpiow <49477059+DRpiow@users.noreply.github.com>
2019-04-12 10:36:54 +0700

    Update play.js

--- Command/play.js
+++ Command/play.js
@@ -1,31 +1,60 @@
-   // first, we need to check if the auther is connected to a voice channel
-  if (!message.member.voiceChannel) return message.channel.send('Please connect to a voice channel.');
-  // if not, return & send a message to chat
-  
-  // Check if bot is already connected to a voice channel
-  if (message.guild.me.voiceChannel) return message.channel.send('sorry, the bot is already connected to the guild.');
-  
-  // Check if auther input a url
-  if (!args[0]) return message.channel.send('Sorry, please input a url following the command.');
-  
-  // Validate Info
-  let validate = await ytdl.validateURL(args[0]);
-  
-  // Check validation 
-  if (!validate) return message.channel.send('Sorry, Please input a **valid** url following the command.');
-  // validate will contain a boolean if the url is valid or not
-  
-  // Fetch video info
-  let info = await ytdl.getInfo(args[0]);
-  
-  // Store authors guild channel
-  let connect = await message.member.voiceChannel.join();
-  
-  // Play song
-  let dispatcher = await connection.play(ytdl(args[0], {filter: 'audioonly' }));
-  // The dispatcher variable will be used in a later episode
-  
-  // Output now playing
-  message.channel.send('Now playing: ${info.title}');
-  
-} // Now, we can test it!
+  const commando = require('discord.js-commando');
+const YTDL = require('ytdl-core');
+
+function Play(connection, message)
+{
+    var server = servers[message.guild.id];
+    server.dipatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dipatcher.on("end", function(){
+        if(server.queue[0])
+        {
+            Play(connection,message);
+        }
+        else
+        {
+            connection.disconect();
+        }
+    });            
+
+}
+
+class JoinChannelCommand extends commando.Command
+{
+    constructor(client)  
+    {
+        super(client,{
+            name: 'join',
+            group: 'music',
+            memberName: 'join',
+            description: 'Joins the voice channel of the commander
+        });
+    }
+
+    async run(message, args)
+    {
+        if(message.member.voiceChannel)
+        {
+            if(!message.guild.voiceConnection)
+            {
+                if(!server[message.guild.id])
+                {
+                    server[message.guild.id] = {queue: []}
+                }
+                message.member.voiceChannel.join()
+                    .then(connection =>{
+                        var server = server[message.guild.id];
+                        message.replay("Successfully Joined!");
+                        server.queue.push(args);
+                        Play(connection, message);
+                    })       
+            }
+        }
+        else
+        {
+            message.replay("You must be in a voice channel to summon me!");
+        }
+    }
+}
+
+module.exports = JoinChannelCommand;
